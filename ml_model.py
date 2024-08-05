dccda2c59e73d7563569e0d33b09e0d2697b345e
chetanrajhans <chetanrajhans2010@gmail.com>
2024-08-05 23:25:08 +0530

    added 3 more model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding ML related code here")
+
+model1():
+    print("addind code for model 1")
+
+model2():
+    print("addind code for model 2")
+
+model3():
+    print("addind code for model 3")
